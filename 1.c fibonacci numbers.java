23dc7a36b26de8499b0d5567bb6a924dbab1c7f5
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-02-08 16:09:52 +0530

    Update 1.c fibonacci numbers.java

--- 1.c fibonacci numbers.java
+++ 1.c fibonacci numbers.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
-class A {
-  public static void main(String[] args)
-  {
-    int term,a=0,b=1,c;
-    System.out.println("Enter term");
-      Scanner sc=new Scanner(System.in);
-     term=sc.nxtInt();
-    for(int i=1;i<=term;i++)
-      {
-        System.out.println(a+" ");
-        c=a+b;
-        a=b;
-        b=c;
-      }
-  }
-}  
+// Print ‘N’ Fibonacci numbers. 
+ import java.util.*; 
+ import java.util.Scanner; 
+ public class Main{ 
+ public static void main(String args[]){ 
+ Scanner in = new Scanner(System.in); 
+ int N, n1=0, n2=1, n3, i; 
+ System.out.println("Enter the value of N : "); 
+ N = in.nextInt(); 
+ System.out.print("Fibonacci sequence upto "+N+" terms : "+n1+" "+n2); 
+ for(i=2;i<N;i++){ 
+ n3 = n1+n2; 
+ System.out.print(" "+n3); 
+ n1 = n2; 
+ n2 = n3; 
+ } 
+ } 
+ }
