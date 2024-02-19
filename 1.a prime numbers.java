ffe55030aebe744cfde33269926f19139493d31d
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-02-19 19:41:30 +0530

    Update 1.a prime numbers.java

--- 1.a prime numbers.java
+++ 1.a prime numbers.java
@@ -3,24 +3,24 @@
  import java.util.Scanner; 
  public class Main{ 
  public static void main(String args[]){ 
- Scanner in = new Scanner(System.in); 
- int n1, n2, i, j, flag; 
- System.out.print("Enter the minimum value : "); 
- n1 = in.nextInt(); 
- System.out.print("Enter the maximum value : "); 
- n2 = in.nextInt(); 
- System.out.println("Prime no. between "+n1+" and "+n2+" are : "); 
- for(i=n1;i<=n2;i++){ 
- if(i==1 || i==0) 
- continue; 
- flag = 1; 
- for(j=2;j<=i/2;j++){ 
- if(i%j == 0){ 
- flag = 0; 
- break; 
+             Scanner in = new Scanner(System.in); 
+             int n1, n2, i, j, flag; 
+             System.out.print("Enter the minimum value : "); 
+             n1 = in.nextInt(); 
+             System.out.print("Enter the maximum value : "); 
+             n2 = in.nextInt(); 
+             System.out.println("Prime no. between "+n1+" and "+n2+" are : "); 
+             for(i=n1;i<=n2;i++){ 
+             if(i==1 || i==0) 
+             continue; 
+             flag = 1; 
+             for(j=2;j<=i/2;j++){ 
+             if(i%j == 0){ 
+             flag = 0; 
+             break; 
  } 
  } 
- if(flag == 1) 
+             if(flag == 1) 
  System.out.print(i+" "); 
  } 
  } 
